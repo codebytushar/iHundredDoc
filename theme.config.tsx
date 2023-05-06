08fd3bc017d019e47a996929cb85cf2c7ccb62f4
tgohil-logilite <tgohil@logilite.com>
2023-05-06 15:57:22 +0530

    Initial Commit

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,9 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
-  project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
-  },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  logo: <span>iHundred</span>,
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'All Rights Reserved, iHundred Development Team.',
   },
 }
 
